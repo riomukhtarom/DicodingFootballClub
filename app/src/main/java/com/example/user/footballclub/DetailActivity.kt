a5d15966838eeca5029a1b9c0fabfe8a4472ae12
Rio Mukhtarom <riomukhtarom10@gmail.com>
2018-08-30 15:48:39 +0800

    Add Recycle View to MainActivity

--- app/src/main/java/com/example/user/footballclub/DetailActivity.kt
+++ app/src/main/java/com/example/user/footballclub/DetailActivity.kt
@@ -2,11 +2,24 @@ package com.example.user.footballclub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import org.jetbrains.anko.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        DetailActivityUI().setContentView(this)
+    }
+
+    class DetailActivityUI: AnkoComponent<DetailActivity>{
+        override fun createView(ui: AnkoContext<DetailActivity>) = with(ui){
+            verticalLayout {
+                padding = dip(16)
+
+            }
+        }
+
     }
 }
