80b3771c682b82840c68565bf749100c2342fd0f
Rio Mukhtarom <riomukhtarom10@gmail.com>
2018-08-30 13:02:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/footballclub/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.user.footballclub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/example/user/footballclub/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.user.footballclub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
